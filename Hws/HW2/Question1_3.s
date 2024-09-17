b2842a8cd4b9f267737164f5b3456f4ff2cd8edf
vsoares <vinicius.malaman12@gmail.com>
2024-09-16 20:49:21 -0500

    Question1_3

---
+++ Hws/HW2/Question1_3.s
@@ -0,0 +1,47 @@
+.data
+str1: .asciiz "E"
+str2: .asciiz "V"
+str3: .asciiz "G"
+str4: .asciiz "S"
+str5: .asciiz "N"
+.align 2
+
+.text
+.globl main
+
+main:
+# Start program
+addi $s1, $zero, 0 # s1 is ouput value
+inputs:
+# Request some user input:
+
+blt $a0, 95, L2
+la $v0, str1
+j EXIT
+L2: blt $a0, 85, L3
+la $v0, str2
+j EXIT
+L3: blt $a0, 75, L4
+la $v0, str3
+j EXIT
+L4: blt $a0, 65, L5
+la $v0, str4
+j EXIT
+la $v0, str5
+
+EXIT:
+
+
+
+
+
+#addu $s1, $s0, $s1
+
+# Print output
+#li $v0, 1
+#addi $a0, $s1, 0
+#syscall
+# Exit program
+li $v0, 10
+syscall
+
