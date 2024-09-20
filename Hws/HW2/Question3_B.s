ba61a786a4e94936f22da3ef94885ee2e6459658
vsoares <vinicius.malaman12@gmail.com>
2024-09-20 12:10:47 -0500

    new QUeston 3_B

--- Hws/HW2/Question3_B.s
+++ Hws/HW2/Question3_B.s
@@ -1,25 +1,54 @@
 .data
-str2: .asciiz "Vinicius"
+str2: .asciiz ""
+dest: .space 20 #allocate space for dest
 .align 2
 .text
 .globl main
-
-main:
-# Start program
-addi $s1, $zero, 0 # s1 is the dest
-addi $s0, $zero, 0 # s0 is the index
-
-Loop: bge $s0, $a1, Exit # if index is greater than the number of characters, exit
-sll $t0, $s0, 2 # multiply index by 4 to get the offset
-lw $t1, $t0($a0) # load the character from the base address + offset
-sw $t1, $t0($s1) # store the character in the dest
-addi $s0, $s0, 1 # increment index
-j Loop # repeat
-Exit:
-
-
-
-
 # Exit program
+main:
+
+    la $a0, dest # Load address of destination
+    la $a1, str2 # Load address of source   
+    # Call strcpy function
+    jal strcpy 
+
+    # Print the copied string
+    li $v0, 4 # syscall for print string
+    la $a0, dest # Load address of destination
+    syscall
+
+
 li $v0, 10
 syscall
+
+
+#function that imitates strcpy, each character is a byte long
+strcpy:
+    # $a0 = destination address
+    # $a1 = source address
+    # a function in MIPS can return up to two values stored prpeviosly in $v0 and $v1 before the jr $ra call.
+LOOP:
+    # Load byte from source
+    lb $t0, 0($a1)
+    # Check if src is null
+    beq $t0,$zero, EXIT
+    # Store byte  in dest
+    sb $t0, 0($a0)
+    # increment pointers
+    addi $a0, $a0, 1
+    addi $a1, $a1, 1
+    # jump
+    j LOOP
+
+EXIT:
+    # setting null terminator
+    sb $zero, 0($a0)
+
+    # Return the destination address
+    move $v0, $a0
+    jr $ra
+
+
+
+
+
