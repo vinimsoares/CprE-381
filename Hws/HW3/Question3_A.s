01a805e8c79ce85e821e42d491fb120b9f623390
vsoares <vinicius.malaman12@gmail.com>
2024-09-25 10:35:13 -0500

    added comments

--- Hws/HW3/Question3_A.s
+++ Hws/HW3/Question3_A.s
@@ -5,7 +5,7 @@
 .globl main
 #THIS PROGRAM CONTAIN A FIBONNACI NUMBER FUNCTION
 main:
-li $a0, 5
+li $a0, 6
 jal fibonacci
 
 li $v0, 1
@@ -29,14 +29,14 @@ addi $s0, $zero, 1
 addi $s1, $zero,   1		# $s1 = zero + 1
 #using $s2 as the n-2 value
 addi	$s2,  $zero, 0
-FOR_LOOP: bge $s0, $a0, exit
-    move $t0,$s1
-    add $s1,$s1,$s2
-    move $s2, $t0
+FOR_LOOP: bge $s0, $a0, exit#if i == N exit
+    move $t0,$s1#save n-1
+    add $s1,$s1,$s2 # n = n-1 + n-2
+    move $s2, $t0 #save the next n-2 with n-1 from this current call
     addi $s0,$s0,1 #increment loop
-    j FOR_LOOP
+    j FOR_LOOP# loop back
 
 exit:
-    move $a0,$s1
-    move $v0, $a0
-    jr $ra
+    move $a0,$s1#save n to argument
+    move $v0, $a0 #save the return value to $v0
+    jr $ra # return
--- Hws/HW3/Question3_B.s
+++ Hws/HW3/Question3_B.s
@@ -27,23 +27,25 @@ jr $ra
 LOOP:
     #open space on stack to save some values
     sub $sp, $sp, 12
-    sw $a0, 0($sp)
-    sw $ra, 4($sp)#save the return address
+    sw $a0, 0($sp)#save n
+    sw $ra, 4($sp)#save the return address !!!VERY IMPORTANT!!!!!
+    #DO NOT FORGET TO SAVE THE RETURN ADDRESS, OR ELSE IT WONT WORK
+    #the function stack call return the last call first and then starts return the calls made before, so you need to Save the address
     #return fibonacci(n-1)
     addi $a0, $a0, -1
     jal fibonacci
     #save the result from this call
     sw $v0, 8($sp)
 
-    lw $t0, 0($sp)
+    lw $t0, 0($sp)#load the original value for this call
     #return fibonacci(n-2)
     addi $a0, $t0, -2
     jal fibonacci
 
-    lw $t0, 8($sp)
-    add $v0, $v0, $t0
-    lw $ra, 4($sp)
-     addi $sp, $sp, 12
-    move $a0, $v0
+    lw $t0, 8($sp)#load the  result from the (n-1) call
+    add $v0, $v0, $t0#n = n-1 + n-2
+    lw $ra, 4($sp)#load the retunr address
+     addi $sp, $sp, 12 # pop the stack
+    move $a0, $v0#move $v0 to $a0 to print
 
-    jr $ra
+    jr $ra # return
